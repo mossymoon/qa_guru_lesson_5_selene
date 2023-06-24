d025b9329beb6aeec47713d3db1df9ac36f483f6
Stepan Sergienko <stephan.sergienko@gmail.com>
2023-06-24 11:28:44 +0300

    updated version

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,14 +1,14 @@
+import os
+
 from selene import browser, be, have
 from selene.core import command
 from selene.support.shared.jquery_style import s
 
 
 def test_dificult_form():
-    picture = '/Users/stepa/PycharmProjects/qa_guru_lesson_5_selene/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg'
     browser.open('automation-practice-form')
 
     # заполнение формы
-
     s('#adplus-anchor').perform(command.js.remove)
     s('#fixedban').perform(command.js.remove)
 
@@ -20,18 +20,20 @@ def test_dificult_form():
 
     browser.element('[id="dateOfBirthInput"]').click()
     browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select [value="1989"]').click()
-    browser.element('.react-datepicker__month-select [value="12"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('[value="1989"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('[value="11"]').click()
     browser.element('.react-datepicker__day--028').click()
-
-    browser.element('[id="subjectsInput"]').type("Maths").press_enter()
+    browser.element('[id="subjectsInput"]').click().send_keys("Maths").press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('input[type=file]').type(picture)
+    browser.element('#uploadPicture').send_keys(os.path.abspath('tests/images/picture.jpeg'))
     browser.element('[id=currentAddress]').type('Москва, ул. Тверская, дом 1')
     browser.element('input#react-select-3-input').type("Haryana").press_enter()
     browser.element('input#react-select-4-input').type("Panipat").press_enter()
     browser.element('[id="submit"]').click()
     browser.element('[id="closeLargeModal"]').click()
+
 
     # Проверка формы
     browser.element('.modal-header').should(have.exact_text('Thanks for submitting the form'))
@@ -50,4 +52,3 @@ def test_dificult_form():
     # Закрытие модального окна
     browser.element('#closeLargeModal').click()
     browser.element('.modal-dialog').should(be.absent)
-    browser.element('#firstName').should(be.blank)
